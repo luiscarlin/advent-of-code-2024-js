0557eec4fa75ec17f6a5f9365cb90d57986ff6a5
Luis Carlin <luiscarlin@gmail.com>
2024-11-30 23:59:42 -0500

    start day1

---
+++ day01/day01.mjs
@@ -0,0 +1,16 @@
+import fs from 'node:fs';
+
+function part1() {
+  const data = fs.readFileSync('./day01/file.in', 'utf8');
+  // start here
+  console.log('part 1', 'not yet implemented');
+}
+
+function part2() {
+  const data = fs.readFileSync('./day01/file.in', 'utf8');
+  // start here
+  console.log('part 2', 'not yet implemented');
+}
+
+part1();
+part2();
