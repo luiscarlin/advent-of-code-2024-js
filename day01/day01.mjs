dcae6d102aa52223ff8208a08e2a7436bfb9d9c9
Luis Carlin <luiscarlin@gmail.com>
2024-12-01 00:11:11 -0500

    day 1 part 1 done

--- day01/day01.mjs
+++ day01/day01.mjs
@@ -1,9 +1,32 @@
+import { add } from 'mathjs';
 import fs from 'node:fs';
 
 function part1() {
-  const data = fs.readFileSync('./day01/file.in', 'utf8');
-  // start here
-  console.log('part 1', 'not yet implemented');
+  let left = [];
+  let right = [];
+
+  const data = fs
+    .readFileSync('./day01/file.in', 'utf8')
+    .split('\n')
+    .filter(Boolean);
+
+  data.forEach((line, index) => {
+    const [l, r] = line.split('  ');
+
+    left.push(Number(l));
+    right.push(Number(r));
+  });
+
+  left = left.sort();
+  right = right.sort();
+
+  const diff = [];
+
+  for (let i = 0; i < left.length; i++) {
+    diff.push(Math.abs(left[i] - right[i]));
+  }
+
+  console.log('part 1', add(...diff));
 }
 
 function part2() {
