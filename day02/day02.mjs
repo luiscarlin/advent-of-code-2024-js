1a3615fcd6d6453f7146c8ffc7e25ca49c92be10
Luis Carlin <luiscarlin@gmail.com>
2024-12-02 07:25:54 -0500

    day 2 part 1 done

---
+++ day02/day02.mjs
@@ -0,0 +1,56 @@
+import { log } from 'node:console';
+import fs from 'node:fs';
+
+function part1() {
+  const lines = fs
+    .readFileSync('./day02/file.in', 'utf8')
+    .split('\n')
+    .filter(Boolean)
+    .map((line) => line.split(' ').map(Number));
+
+  let linesSafe = 0;
+
+  for (const line of lines) {
+    let rowIncreasing;
+
+    for (let i = 0; i < line.length; i++) {
+      if (i === 0) {
+        continue;
+      }
+
+      const prev = line[i - 1];
+      const curr = line[i];
+
+      const diff = curr - prev;
+
+      if (Math.abs(diff) > 3 || diff === 0) {
+        break;
+      }
+
+      const increasing = diff > 0;
+
+      if (i === 1) {
+        rowIncreasing = increasing;
+      }
+
+      if (rowIncreasing !== increasing) {
+        break;
+      }
+
+      if (i === line.length - 1) {
+        linesSafe++;
+      }
+    }
+  }
+
+  log('part 1', linesSafe);
+}
+
+function part2() {
+  const lines = fs.readFileSync('./day02/file.in', 'utf8');
+  // start here
+  log('part 2', 'not yet implemented');
+}
+
+part1();
+part2();
