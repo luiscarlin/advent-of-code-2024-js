c36597dc3c7597f4c7c2277262e780893892a3fb
Luis Carlin <luiscarlin@gmail.com>
2024-12-07 00:56:41 -0500

    day 7 part 1 done

--- day07/day07.mjs
+++ day07/day07.mjs
@@ -2,9 +2,47 @@ import { log } from 'node:console';
 import fs from 'node:fs';
 
 function part1() {
-  const lines = fs.readFileSync('./day07/file.in', 'utf8');
-  // start here
-  log('part 1', 'not yet implemented');
+  const lines = fs.readFileSync('./day07/file.in', 'utf8').trim().split('\n');
+
+  // find if there is an equation combination that is valid
+  const isEquationValid = (result, nums) => {
+    // accumulate final result in nums[0]
+    if (nums.length === 1) {
+      // if the result is the same as the first number, then it's valid
+      return result === nums[0];
+    }
+
+    // more than 1 number, so reduce the array
+
+    // reduce by adding the first two numbers
+    const addedNums = [nums[0] + nums[1]].concat(nums.slice(2));
+    if (isEquationValid(result, addedNums)) {
+      return true;
+    }
+
+    // reduce by multiplying the first two numbers
+    const multipliedNums = [nums[0] * nums[1]].concat(nums.slice(2));
+    if (isEquationValid(result, multipliedNums)) {
+      return true;
+    }
+
+    return false;
+  };
+
+  let total = 0;
+
+  for (const line of lines) {
+    let [result, nums] = line.split(': ');
+
+    result = Number(result);
+    nums = nums.split(' ').map(Number);
+
+    if (isEquationValid(result, nums)) {
+      total += result;
+    }
+  }
+
+  log('part 1', total);
 }
 
 function part2() {
